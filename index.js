95c2ac83767c5edc6f7991cf13d3d8e05436c64b
EErriicCC <122754188+EErriicCC@users.noreply.github.com>
2023-01-22 19:27:37 +0100

    Update

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-
-
 const ecuationSolver = function(){
-    const a = document.getElementById("a")
-    const b = document.getElementById("b")
-    const c = document.getElementById("c")
-    let s1 = document.getElementById("solution1")
-    let s2 = document.getElementById("solution2")
-    let discriminant = Math.sqrt(b.value ** 2- 4 * a.value * c.value);
-    let x1 = (-b.value + discriminant) / (2*a.value); 
-    let x2 = (-b.value - discriminant) / (2*a.value);
-    s1.innerText = x1;
-    s2.innerText = x2;
+    const a = parseFloat(document.getElementById("a").value);
+    const b = parseFloat(document.getElementById("b").value);
+    const c = parseFloat(document.getElementById("c").value);
+    let s1 = document.getElementById("solution1");
+    let s2 = document.getElementById("solution2");
+
+    if(isNaN(a) || isNaN(b) || isNaN(c)){
+        s1.innerText = "Por favor ingrese valores numéricos válidos";
+        s2.innerText = '';
+        return;
+    }
+    if(a == 0){
+        s1.innerText = "El valor de a no puede ser cero";
+        s2.innerText = '';
+        return;
+    }
+
+    let discriminant = (b * b- 4 * a * c);
+    if (discriminant<0){
+        let x1 = -b / (2*a);
+        let x2 = Math.sqrt(-discriminant) / (2*a);
+        s1.innerHTML = `Soluciones imaginarias: <br> x1= ${x1} + <i>${x2}i</i>, <br> x2= ${x1} - <i>${x2}i</i>`;
+        s2.innerText = '';
+    } else {
+        discriminant = Math.sqrt(discriminant);
+        let x1 = (-b + discriminant) / (2*a); 
+        let x2 = (-b - discriminant) / (2*a);
+        s1.innerText = x1;
+        s2.innerText = x2;
+    }
 }
-
-
