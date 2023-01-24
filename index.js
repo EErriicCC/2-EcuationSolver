cd7874b4ac9be3e102035258e5eff3c4d75027ba
EErriicCC <122754188+EErriicCC@users.noreply.github.com>
2023-01-24 20:49:35 +0100

    update

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 const ecuationSolver = function(){
-    const a = parseFloat(document.getElementById("a").value);
-    const b = parseFloat(document.getElementById("b").value);
-    const c = parseFloat(document.getElementById("c").value);
-    let s1 = document.getElementById("solution1");
-    let s2 = document.getElementById("solution2");
+    let aInput = document.getElementById("a");
+    let bInput = document.getElementById("b");
+    let cInput = document.getElementById("c");
+    const Solver = function(){
 
-    if(isNaN(a) || isNaN(b) || isNaN(c)){
-        s1.innerText = "Por favor ingrese valores numéricos válidos";
-        s2.innerText = '';
-        return;
-    }
-    if(a == 0){
-        s1.innerText = "El valor de a no puede ser cero";
-        s2.innerText = '';
-        return;
-    }
+        let s1 = document.getElementById("solution1");
+        let s2 = document.getElementById("solution2");
+        const aValue = parseFloat(aInput.value);
+        const bValue = parseFloat(bInput.value);
+        const cValue = parseFloat(cInput.value);
 
-    let discriminant = (b * b- 4 * a * c);
-    if (discriminant<0){
-        let x1 = -b / (2*a);
-        let x2 = Math.sqrt(-discriminant) / (2*a);
-        s1.innerHTML = `Soluciones imaginarias: <br> x1= ${x1} + <i>${x2}i</i>, <br> x2= ${x1} - <i>${x2}i</i>`;
-        s2.innerText = '';
-    } else {
-        discriminant = Math.sqrt(discriminant);
-        let x1 = (-b + discriminant) / (2*a); 
-        let x2 = (-b - discriminant) / (2*a);
-        s1.innerText = x1;
-        s2.innerText = x2;
+
+        if(isNaN(aValue) || isNaN(bValue) || isNaN(cValue)){
+            s1.innerText = "Por favor ingrese valores numéricos válidos";
+            s2.innerText = '';
+            return;
+        }
+
+        if(aValue == 0){
+            s1.innerText = "El valor de a no puede ser cero";
+            s2.innerText = '';
+            return;
+        }
+
+        let discriminant = (bValue * bValue - 4 * aValue * cValue);
+        if (discriminant<0){
+            let x1 = -bValue / (2*aValue);
+            let x2 = Math.sqrt(-discriminant) / (2*aValue);
+            s1.innerHTML = `Soluciones imaginarias: <br> x1= ${x1} + <i>${x2}i</i>, <br> x2= ${x1} - <i>${x2}i</i>`;
+            s2.innerText = '';
+        } else {
+            discriminant = Math.sqrt(discriminant);
+            let x1 = (-bValue + discriminant) / (2*aValue); 
+            let x2 = (-bValue - discriminant) / (2*aValue);
+            s1.innerText = x1;
+            s2.innerText = x2;
+        }
     }
+    aInput.addEventListener('input',Solver);
+    bInput.addEventListener('input',Solver);
+    cInput.addEventListener('input',Solver);
 }
